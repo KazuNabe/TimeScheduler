8f01378d9e5312bc85c5543442d187142924501f
WatanabeKazuya <Kazuya@Kaz-PC.local>
2016-02-22 16:32:34 +0900

    first commit

---
+++ TimeScheduler/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  TimeScheduler
+//
+//  Created by WatanabeKazuya on 2016/02/22.
+//  Copyright © 2016年 jp.studio.edamame. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
