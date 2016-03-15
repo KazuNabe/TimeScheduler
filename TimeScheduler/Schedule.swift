8a7441eadc1644a81fcc715d2dd049f01d4c6e52
Kazuya Watanabe <Kazuya@Kaz-PC.local>
2016-03-15 23:38:16 +0900

    Schedule model class inherits model

--- TimeScheduler/Schedule.swift
+++ TimeScheduler/Schedule.swift
@@ -9,28 +9,18 @@
 import UIKit
 import RealmSwift
 
-class Schedule: Object {
-    dynamic var guid : String = ""
-    
+class Schedule: Model {
     dynamic var eventTitle : String = ""
     dynamic var eventDetail : String = ""
     
     dynamic var eventStartTime : NSDate?
     dynamic var eventEndTime : NSDate?
     
-    override static func primaryKey() -> String {
-        return "guid";
-    }
-    
-    private static func createGuid() -> String {
-        return NSUUID().UUIDString
-    }
-    
     static func create() -> Schedule {
         let schedule = Schedule()
         
         schedule.guid = createGuid()
         
-        return schedule;
+        return schedule
     }
 }
