fa41b3660e23745db62347eaf1d0af0fb0b9dc8e
Kazuya Watanabe <Kazuya@Kaz-PC.local>
2016-03-21 19:05:07 +0900

    Add circleSchedule view

---
+++ TimeScheduler/CircleScheduleView.swift
@@ -0,0 +1,21 @@
+//
+//  CircleScheduleView.swift
+//  TimeScheduler
+//
+//  Created by Kazuya Watanabe on 2016/03/21.
+//  Copyright © 2016年 jp.studio.edamame. All rights reserved.
+//
+
+import UIKit
+
+class CircleScheduleView: UIView {
+
+    /*
+    // Only override drawRect: if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func drawRect(rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
